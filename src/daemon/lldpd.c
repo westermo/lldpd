cf8bb45e6ae9c79df2465640465a3d8a53e1c0f3
Jonas Johansson <jonas.johansson@westermo.se>
2018-03-13 09:51:13 +0100

    Only set LLDP_CAP_BRIDGE and LLDP_CAP_ROUTER as available capabilities
    Signed-off-by: Jonas Johansson <jonas.johansson@westermo.se>

--- src/daemon/lldpd.c
+++ src/daemon/lldpd.c
@@ -1879,8 +1879,7 @@ lldpd_main(int argc, char *argv[], char *envp[])
 		calloc(1, sizeof(struct lldpd_chassis))) == NULL)
 		fatal("localchassis", NULL);
 	cfg->g_config.c_cap_advertise = 1;
-	lchassis->c_cap_available = LLDP_CAP_BRIDGE | LLDP_CAP_WLAN |
-	    LLDP_CAP_ROUTER | LLDP_CAP_STATION;
+	lchassis->c_cap_available = LLDP_CAP_BRIDGE | LLDP_CAP_ROUTER;
 	cfg->g_config.c_mgmt_advertise = 1;
 	TAILQ_INIT(&lchassis->c_mgmt);
 #ifdef ENABLE_LLDPMED
